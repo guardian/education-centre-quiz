ba74f04a24f5efa885e1b955aef8ff6fe5a37125
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-09-20 17:40:50 +0100

    Updated template to on load javascript. A method to check an answer with student to work out how to render this back to user. Next steps to work with multiple questions

--- src/quiz.js
+++ src/quiz.js
@@ -1,22 +1,30 @@
 function Question (id) {
 	// body...
 	var question = document.getElementById(id)
-
-	this.setQuestion = function (question) {
-		question.querySelector('.question').innerHTML = question;
+	this.setQuestion = function (questionText) {
+		question.querySelector('.question').innerHTML = questionText;
 	};
 
-	this.setAnswers = function (answer1, answer2) {
-		// TODO
+	this.setAnswers = function (answer1, answer2, answer3) {
+		//TODO
 	};
-
-	
-
-	body.appendChild(sxdasda);
 }
 
-var q1 = new Question("question1");
+var answers = [];
+function checkAnswer() {
+    var selectedAnswer = document.querySelector('input[name="q1Choices"]:checked').value;
+    if(selectedAnswer == answers[0]) alert("Correct answer");
+    else alert("Incorrect answer");                                      
+    
+}
+window.onload = function() {
 
-q1.setQuestion('The question');
+    var q1 = new Question("question1");
 
-q1.setAnswers('An answer', 'answer 2');
+    q1.setQuestion('When was the Guardian first published?');
+
+    q1.setAnswers('1791', '1821', '1999');
+    
+    this.answers.push('answer2');
+
+}
