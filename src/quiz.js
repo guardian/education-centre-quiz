61e0a4bd1c7ca81b2ad60bee17290322d83e7c67
Will Franklin <will.franklin@guardian.co.uk>
2014-10-31 16:14:38 +0000

    Basic for creating question, with student setting answers

--- src/quiz.js
+++ src/quiz.js
@@ -1,44 +1,28 @@
-var form = document.getElementById('quiz');
+var quiz = document.querySelector('#quiz');
+var questions = quiz.querySelector('#questions');
+var result = quiz.querySelector('#result');
 
-function Question () {
-    var question = document.getElementById('template').cloneNode(true);
-    form.appendChild(question);
-    
+function copyTemplate(id, to) {
+    var element = document.querySelector(id).cloneNode(true);
+    to.appendChild(element);
+    return element;
+}
+
+function Question() {
+    var question = copyTemplate('#question-template', questions);
+
     this.setQuestion = function (questionText) {
-		question.querySelector('.question').textContent = questionText;
-	};
+        question.querySelector('.question').textContent = questionText;
+    };
 
-	this.setAnswers = function (answer1, answer2, answer3) {
+    this.setAnswers = function (answer1, answer2, answer3) {
         //Student todo
         question.querySelector('.answer1').textContent = answer1;
         question.querySelector('.answer2').textContent = answer2;
         question.querySelector('.answer3').textContent = answer3;
-	};
-    
-    this.checkAnswer = function (value) {
-        var selectedAnswer = question.querySelector(':checked').value;
-        //Student todo
-        return selectedAnswer === value;
     };
 }
 
-form.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-
-    //Student todo
-    var result = form.querySelector('.result');
-    if (q1.checkAnswer("answer2")) {
-        result.textContent = "Hooray";
-    } else {
-        result.textContent = "Boo";
-    }
-    
-});
-
-
 var q1 = new Question();
-
 q1.setQuestion('When was the Guardian first published?');
-
 q1.setAnswers('1791', '1821', '1999');
-
