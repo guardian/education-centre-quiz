76561cd8e7c6bf36c4acaf8c5d3a70b634a9b7c1
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-01-05 17:53:42 +0000

    hiding functions away

--- src/quiz.js
+++ src/quiz.js
@@ -2,33 +2,57 @@ var quiz = document.querySelector('#quiz');
 var questions = quiz.querySelector('#questions');
 var result = quiz.querySelector('#result');
 
-function copyTemplate(id, to) {
-    var element = document.querySelector(id).cloneNode(true);
-    element.id = '';
-    to.appendChild(element);
-    return element;
-}
-
 function Question() {
-    var question = copyTemplate('#question-template', questions);
-    var answers = question.querySelector('.answers');
     var correctAnswer;
  
     this.setQuestion = function (questionText) {
-        question.querySelector('.question').textContent = questionText;
+        var questionElement = getQuestionContainer();
+        setText(questionElement, questionText);
     };    
   
     this.setAnswers = function (answer1, answer2, answer3) {
-    
+       var answerElement = createAnswerContainer();
+        setText(answerElement, answer1);
+        setRadioButton(answerElement, 'blah', answer1);
     };
 
     this.setCorrectAnswer = function () {
-        
+
     };
     
     this.checkAnswer = function () {
         
     };
+
+    ////////////This code is helper functions
+    var questionContainer = copyTemplate('#question-template', questions);
+    var answersContainer = questionContainer.querySelector('.answers');
+
+    function  getQuestionContainer() {
+        console.log(questionContainer, questionContainer.querySelector('.question'));
+        return questionContainer.querySelector('.question');
+    }
+
+    function createAnswerContainer() {
+        return copyTemplate('#answer-template', answersContainer);
+    }
+
+    function setText(element, textToSet) {
+        element.querySelector('.text').textContent = textToSet;
+    }
+
+    function setRadioButton(element, name, value) {
+        var radioButtonElement = element.querySelector('.choice');
+        radioButtonElement.setAttribute('name', name);
+        radioButtonElement.setAttribute('value', value);
+    }
+
+    function copyTemplate(id, to) {
+        var element = document.querySelector(id).cloneNode(true);
+        element.id = '';
+        to.appendChild(element);
+        return element;
+    }
 }
 
 quiz.addEventListener('submit', function (evt) {
@@ -42,6 +66,6 @@ quiz.addEventListener('submit', function (evt) {
 
 var q1 = new Question();
 q1.setQuestion('When was the Guardian first published?');
-q1.setAnswers(['1791', '1821', '1999']);
+q1.setAnswers('1791', '1821', '1999');
 q1.setCorrectAnswer('1821');
 
