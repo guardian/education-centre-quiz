2916f550c50581e58c2eeeb0412955774a7c1c61
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-01-07 22:31:20 +0000

    full guide for ed centre

--- src/quiz.js
+++ src/quiz.js
@@ -10,12 +10,46 @@ function Question() {
         setText(questionElement, questionText);
     };    
   
+    //first part will be to add the answers  
     this.setAnswers = function (answer1, answer2, answer3) {
-       var answerElement1 = createAnswerContainer();
+        var answerElement1 = createAnswerContainer();
         setText(answerElement, answer1);
         setRadioButton(answerElement1, 'q1Choices', answer1);
+
+        var answerElement2 = createAnswerContainer();
+        setText(answerElement, answer2);
+        setRadioButton(answerElement2, 'q1Choices', answer2);
+
+        var answerElement3 = createAnswerContainer();
+        setText(answerElement3, answer1);
+        setRadioButton(answerElement3, 'q1Choices', answer3);
     };
 
+    //second part to distinguish between radio buttons
+    this.setAnswers = function (answer1, answer2, answer3, choiceName) {
+        var answerElement1 = createAnswerContainer();
+        setText(answerElement, answer1);
+        setRadioButton(answerElement1, choiceName, answer1);
+
+        var answerElement2 = createAnswerContainer();
+        setText(answerElement, answer2);
+        setRadioButton(answerElement2, choiceName, answer2);
+
+        var answerElement3 = createAnswerContainer();
+        setText(answerElement3, answer1);
+        setRadioButton(answerElement3, choiceName, answer3);
+    };
+
+    //third part to send a list and write a for loop
+    this.setAnswers = function (answers, choiceName) {
+
+        for(i=0; i< answers.length; i++) {
+            var answerElement = createAnswerContainer();
+            setText(answerElement, answers[i]);
+            setRadioButton(answerElement, choiceName, answers[i]);
+
+        }
+    };
 
     ////////////This code is helper functions
     var questionContainer = copyTemplate('#question-template', questions);
@@ -27,6 +61,10 @@ function Question() {
 
     function createAnswerContainer() {
         return copyTemplate('#answer-template', answersContainer);
+    }
+
+    function  getAnswerContainer() {
+        return quiz.querySelector('#result');
     }
 
     function setText(element, textToSet) {
@@ -53,6 +91,7 @@ function Question() {
     
     this.checkAnswer = function () {
         var selectedAnswer = questionContainer.querySelector(':checked').value;
+        //student should write this?
         return selectedAnswer === correctAnswer;
     };
     //// end of helper functions
@@ -61,15 +100,39 @@ function Question() {
 quiz.addEventListener('submit', function (evt) {
     evt.preventDefault();
     var result = quiz.querySelector('#result');
-    
-    result.innerHTML = "You got 2 out of 2 correct";
+
+    //last check number of answers correct
+    var counter = 0;
+    if(q1.checkAnswer()) {
+        counter++;
+    }
+    if(q2.checkAnswer()) {
+        counter++;
+    }
+    alert(counter);
+    result.innerHTML = "You got " + counter + " out of 2 correct";
 });
 
 
 
 var q1 = new Question();
 q1.setQuestion('When was the Guardian first published?');
-//make variable list first then pass in
 q1.setAnswers('1791', '1821', '1999');
+//second part (for grouping by radio button)
+q1.setAnswers('1791', '1821', '1999', "q1Choices");
+//third part is to allow any number of choices by creating a list and using a for loop
+var choices = ['1791', '1821', '1999'];
+q1.setAnswers(choices, "q1Choices");
+
 q1.setCorrectAnswer('1821');
 
+var q2 = new Question();
+q2.setQuestion('When was the Guardian first online?');
+q2.setAnswers('1995', '1990', '1999'); //add another question - radio buttons are all grouped together
+//So we must change setAnswers to take another parameter that will group the radio buttons by answer
+q2.setAnswers('1995', '1990', '1999', 'q2Choices');
+//third part is to allow any number of choices by creating a list and using a for loop
+var choices = ['1995', '1990', '1999'];
+q2.setAnswers(choices, "q2Choices");
+
+q2.setCorrectAnswer('1995');
