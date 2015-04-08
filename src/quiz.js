bc212e86511b4875730782f64ff13cd30628e826
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-04-08 16:35:29 +0100

    Add image per question

--- src/quiz.js
+++ src/quiz.js
@@ -7,6 +7,7 @@ function Question() {
      //The code below contains helper functions -- you don't need to edit this code
     var questionContainer = copyTemplate('#question-template', questions);
     var answersContainer = questionContainer.querySelector('.answers');
+    var imageContainer = questionContainer.querySelector('.imageForQuestion')
 
     function  getQuestionContainer() {
         return questionContainer.querySelector('.question');
@@ -14,6 +15,10 @@ function Question() {
 
     function createAnswerContainer() {
         return copyTemplate('#answer-template', answersContainer);
+    }
+
+    function createImageContainer() {
+        return copyTemplate('#image-template', imageContainer);
     }
 
     function setText(element, textToSet) {
@@ -24,6 +29,11 @@ function Question() {
         var radioButtonElement = element.querySelector('.choice');
         radioButtonElement.setAttribute('name', name);
         radioButtonElement.setAttribute('value', value);
+    }
+
+    function setImage(element, source) {
+        var imageElement = element.querySelector('.image');
+        imageElement.setAttribute('src', source)
     }
 
 
@@ -51,6 +61,11 @@ function Question() {
         setText(answerElement1, answerList[0]);
         setRadioButton(answerElement1, 'q1Choices', answerList[0]);
     };
+
+    this.setImage = function (source) {
+       var answerElement1 = createImageContainer();
+        setImage(answerElement1, source);
+    };
    
 }
 
@@ -69,4 +84,5 @@ var q1 = new Question();
 q1.setQuestion('When was the Guardian first published?');
 var choices = ['1791', '1821', '1999']
 q1.setAnswers(choices);
+q1.setImage("https://media.guim.co.uk/f8f71e61c77b03ba9f5c674ba5449f3f1d1d4b51/21_226_3362_2016/140.jpg");
 
